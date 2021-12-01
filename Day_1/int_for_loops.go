49a41cba277ad5a0390825e58d2055ce634607ac
amills157 <alanmills88@gmail.com>
2021-12-01 17:16:50 +0000

    Tidy up spacing horror show

--- Day_1/int_for_loops.go
+++ Day_1/int_for_loops.go
@@ -1,30 +1,30 @@
 package main
 import (
-    "bufio"
-    "fmt"
+	"bufio"
+	"fmt"
 	"os"
 	"log"
-    "strconv"
+	"strconv"
 )
 
 func ReadInts(r string) ([]int, error) {
 	file, err := os.Open(r)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    scanner.Split(bufio.ScanWords)
-    var result []int
-    for scanner.Scan() {
-        x, err := strconv.Atoi(scanner.Text())
-        if err != nil {
-            return result, err
-        }
-        result = append(result, x)
-    }
-    return result, scanner.Err()
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+	var result []int
+	for scanner.Scan() {
+		x, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return result, err
+		}
+		result = append(result, x)
+	}
+	return result, scanner.Err()
 }
 
 func puzzleOne(arr []int) int {
@@ -52,27 +52,25 @@ func puzzleTwo(arr []int) []int {
 		}
 	}
 
-	return 	array_sums;
+return 	array_sums;
 }
 
 func main() {
 	var increase_count, increase_count_2 int
 
-    puzzle_one_array, err := ReadInts("puzzle_1.txt")
+	puzzle_one_array, err := ReadInts("puzzle_1.txt")
 	if err != nil {
-        log.Fatal(err)
-    }
-	
-    increase_count = puzzleOne(puzzle_one_array)
+		log.Fatal(err)
+	}
+
+	increase_count = puzzleOne(puzzle_one_array)
 
 	puzzle_two_array := puzzleTwo(puzzle_one_array)
 
-	/* output the returned value */
 	fmt.Printf( "Puzzle 1 answer is: %d\n", increase_count );
 
 	increase_count_2 = puzzleOne(puzzle_two_array)
 
 	fmt.Printf( "Puzzle 2 answer is: %d\n", increase_count_2 );
-
 
 }
