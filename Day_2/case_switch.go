1b3f90f6f1a31b7e6695908f9fd80eee5d2d6223
amills157 <alanmills88@gmail.com>
2021-12-02 18:18:27 +0000

    rename file

--- Day_2/case_switch.go
+++ Day_2/case_switch.go
@@ -50,46 +50,6 @@ func Both_Puzzles(r string) {
 	return
 }
 
-/*
-func Puzzle_2(r string) {
-	var split_str []string
-	var horizontal_pos, forward_pos, aim int
-
-	file, err := os.Open(r)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
-	for scanner.Scan() {
-		x := scanner.Text()
-		split_str = strings.Split(x, " ")
-		value, err := strconv.Atoi(split_str[1])
-		if err != nil {
-			return
-		}
-		
-		switch split_str[0] {
-		case "forward":
-			forward_pos += value
-			horizontal_pos += aim*value
-		case "up":
-			aim -= value
-		case "down":
-			aim += value
-		default:
-			fmt.Println("Here be dragons")
-		}
-	}
-
-	fmt.Printf( "Puzzle 2 answer is: %d\n", forward_pos * horizontal_pos );
-	
-	return
-}
-*/
-
 func main() {
 
 	Both_Puzzles("puzzle_1.txt")
